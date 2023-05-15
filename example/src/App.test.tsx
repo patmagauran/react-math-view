e543a523da3de61b6d099a1a9ae6b2aaed08a19f
Patrick Magauran <patmagauran.j@gmail.com>
2023-05-15 12:34:32 -0400

    Force upgrade and rework dependency tree

--- example/src/App.test.tsx
+++ example/src/App.test.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import ReactDOM from 'react-dom';
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
